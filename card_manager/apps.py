a7fc6eb73ce4afbff33e26e23824dafaf2c70588
teta <covo@net.net>
2024-12-20 17:04:58 +0500

    change of project structure

--- card_manager/apps.py
+++ card_manager/apps.py
@@ -5,3 +5,4 @@ class CardManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'card_manager'
     verbose_name = 'Card manager'
+    path = '../card_manager'
