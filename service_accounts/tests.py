27e5684fa27c132be9694d28a9181d244cd53b6a
teta <covo@net.net>
2025-01-20 16:33:55 +0500

    start

--- service_accounts/tests.py
+++ service_accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .customuser import CustomUser
+import json
 
-# Create your tests here.
+class TestAccountSystem(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='TestUser', 
+                                              password='password', 
+                                              email='test@test.test')
+        self.example_data = {
+          'login': 'login',
+          'password': 'password',
+          'email': 'email',
+          'rememberMe': True
+        }
+
+    def test_registration(self):
+        request_data = self.example_data
+        
+        response = self.client.post(
+            reverse('reg'),
+            data=json.dumps(request_data),
+            content_type='application/json'
+        )
