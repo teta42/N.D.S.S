1dc05450b30373c554671626343cc1d64fe349bf
Teta <example@example.com>
2025-01-22 16:26:21 +0500

    improved display in the admin panel

--- card_manager/admin.py
+++ card_manager/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Note
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('note_id', 'user_id', 'read_count')
+    search_fields = ('user_id', 'content') # 'title'
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+admin.site.register(Note,  NoteAdmin)
